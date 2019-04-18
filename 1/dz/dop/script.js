6eb1f626cb2fccbcb90c8f89fcbc5f868130509d
Андрей <anderson110475@gmail.com>
2019-04-18 21:28:06 +0300

    first commit

--- 1/dz/dop/script.js
+++ 1/dz/dop/script.js
@@ -1,15 +1,17 @@
-'use strict'
-let num = 33721;
-
-let methNow = 3*3*7*2*1;
-
-let methToCube = methNow;
-
-
-
+'use strict';
+let num = 33721,
+    methNow = 3*3*7*2*1,
+    methToCube = methNow;
+    
 //возведение в степень
 for(let i=0; i<2; i++){
-    methNow *=methToCube
+    methNow *= methToCube;
 }
 
 console.log(methNow);
+console.log(typeof methNow);
+
+for(let i=0; i<2; i++){
+document.write("Символ на позиции " + (i+1) + " " + (''+methNow)[i] + "  ");
+document.write("</br>");
+}
--- 1/dz/dz/script.js
+++ 1/dz/dz/script.js
@@ -1,36 +1,32 @@
-'use strict'
-let money = +prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+'use strict';
+let money = +prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD"),
 
-let timeDate = {
-    budget:money,
-    timeData: time,
-    income:[],
-    savings: false,
-    expenses: {},
-    
-};
+    timeDate = {
+        budget:money,
+        timeData: time,
+        income:[],
+        savings: false,
+        expenses: {},
+        optionalExpenses:{},
+        },
 
-
-let expensesValue = +prompt("Введите обязательную статью расходов в этом месяце");
-let optionalExpenses = +prompt("Введите необязательную статью расходов на этот месяц");
+    expensesValue = prompt("Введите обязательную статью расходов в этом месяце"),
+    optionalExpenses = +prompt("Во сколько обойдётся?");
 
 timeDate.expenses[expensesValue] = optionalExpenses;
 
-let expensesValue2 = +prompt("Введите обязательную статью расходов в этом месяце");
-let optionalExpenses2 = +prompt("Введите необязательную статью расходов на этот месяц");
+let expensesValue2 = prompt("Введите обязательную статью расходов в этом месяце"),
+optionalExpenses2 = +prompt("Во сколько обойдётся?");
 
 timeDate.expenses[expensesValue2] = optionalExpenses2;
 
-let budDay = expensesValue + optionalExpenses + expensesValue2 + optionalExpenses2;
+let budDay = (optionalExpenses + optionalExpenses2);
 
 console.log('Ежедневные траты в день ' + budDay);
 
 let moneyDay = money / 30;
 console.log('Ваш бюджет на день ' + (money/30));
-
-
-
 
 console.log(timeDate);
 
