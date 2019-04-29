faf25d125fa2f43efe7a60a0afe8cb45784d9d24
Андрей <anderson110475@gmail.com>
2019-04-29 22:05:34 +0300

    7 lesson

--- 7/tolesson/script.js
+++ 7/tolesson/script.js
@@ -1,18 +1,31 @@
-let date = new Date(),
-    hh = date.getHours(),
- mm = date.getMinutes(),
- secs = date.getSeconds();
 
- if(hh<10){
-     hh = '0'+hh;
- };
- if(mm<10){
-    mm = '0'+mm;
-};
-if(secs<10){
-    secs = '0'+secs;
-};
+'use strict';
+
 
 let setTimers = document.querySelector(".timers");
-setTimers.textContent = hh+':'+mm+':'+secs;
 
+
+
+
+    
+    
+
+    let timerId = setTimeout(function writeTime(){
+        let date = new Date(),
+    hh = date.getHours(),
+    mm = date.getMinutes(),
+    secs = date.getSeconds();
+
+    if(hh<10){
+        hh = '0'+hh;
+    };
+    if(mm<10){
+        mm = '0'+mm;
+    };
+    if(secs<10){
+        secs = '0'+secs;
+    };
+            console.log('Hello world');
+            setTimers.textContent = hh+':'+mm+':'+secs;
+            setTimeout(writeTime, 1000);
+        });
